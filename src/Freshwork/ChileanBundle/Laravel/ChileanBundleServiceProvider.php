08746f6bf597581c564c64909bbba2c416c0a4c7
Gonzalo Z <gonzunigad@gmail.com>
2014-08-07 03:21:10 -0400

    Se integran Facedes y Alias de Laravel 4

--- src/Freshwork/ChileanBundle/Laravel/ChileanBundleServiceProvider.php
+++ src/Freshwork/ChileanBundle/Laravel/ChileanBundleServiceProvider.php
@@ -1,5 +1,7 @@
-<?php namespace Freshwork\ChileanBundle;
+<?php namespace Freshwork\ChileanBundle\Laravel;
 
+use Freshwork\ChileanBundle\Validations\Rut;
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class ChileanBundleServiceProvider extends ServiceProvider {
@@ -18,8 +20,32 @@ class ChileanBundleServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		//
+
 	}
+
+    public function boot(){
+        \Validator::extend('cl_rut', function($attribute, $value, $parameters)
+        {
+            Rut::$use_exceptions = false;
+            return Rut::validate($value);
+        });
+
+        \App::bind('rut', function()
+        {
+            return new Rut;
+        });
+        //Automatically alias the package.
+        $current_aliases = \Config::get("app.aliases");
+
+        //Get the alias loader instance. Ready to add aliases...
+        $loader = AliasLoader::getInstance();
+
+        //Add Sentry Facade Alias if is not already set on app/config/app.php
+        if (empty($aliases['Rut']))
+        {
+            $loader->alias('Rut', 'Freshwork\ChileanBundle\Facades\Rut');
+        }
+    }
 
 	/**
 	 * Get the services provided by the provider.
